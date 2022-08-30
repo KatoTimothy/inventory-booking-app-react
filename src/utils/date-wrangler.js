a3daf367042be2d3bd4025b4fcd6cc0d0a6be08e
unknown <katotimuci@gmail.com>
2022-08-30 16:36:51 +0300

    Create date-wrangler.js
    Created utility functions to work with dates and weeks

---
+++ src/utils/date-wrangler.js
@@ -0,0 +1,16 @@
+export const shiftDate = (date, daysToShift) => {
+  const clone = new Date(date.getTime());
+  clone.setDate(clone.getDate() + daysToShift);
+  return clone;
+};
+
+export const getWeek = (forDate, daysOffset = 0) => {
+  const date = daysToShift(forDate, daysOffset);
+  const day = date.getDay();
+
+  return {
+    date,
+    start: shiftDate(date, -day),
+    end: shiftDate(date, 6 - day),
+  };
+};
