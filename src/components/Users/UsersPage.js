a01d2b0c535324635e2542d5f8d96c63e7237c19
unknown <katotimuci@gmail.com>
2022-09-07 18:06:45 +0300

    Updated UsersPage component
    The component now renders UserList and UserDetails components

--- src/components/Users/UsersPage.js
+++ src/components/Users/UsersPage.js
@@ -1,8 +1,23 @@
+//import components
+import { useState } from "react";
+import UserDetails from "./UserDetails";
 import UsersList from "./UsersList";
-const UsersPage = () => (
-  <main className="users-page">
-    <UsersList />
-  </main>
-);
+
+//userList component
+const UsersPage = () => {
+  //manage user state
+  const [selectedUser, setSelectedUser] = useState(null);
+
+  // UI
+  return (
+    <main className="users-page">
+      <UsersList
+        selectedUser={selectedUser}
+        setSelectedUser={setSelectedUser}
+      />
+      <UserDetails selectedUser={selectedUser} />
+    </main>
+  );
+};
 
 export default UsersPage;
