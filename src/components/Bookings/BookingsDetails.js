ea5a830895abfbb815cd2b934cba9a1855b9062c
unknown <katotimuci@gmail.com>
2022-09-09 16:46:12 +0300

    Created BookingsDetails component

---
+++ src/components/Bookings/BookingsDetails.js
@@ -0,0 +1,9 @@
+const BookingsDetails = ({ booking, bookable }) => {
+  return (
+    <div className="booking-details placeholder">
+      <p>Bookings Details</p>
+    </div>
+  );
+};
+
+export default BookingsDetails;
