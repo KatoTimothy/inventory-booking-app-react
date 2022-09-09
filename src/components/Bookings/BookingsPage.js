9e1dfd1f923d3cfbbd89eb95c61a8c54cdc3623e
unknown <katotimuci@gmail.com>
2022-09-09 16:47:41 +0300

    Update BookingsPage.js
    The component now renders BookingsList and Bookings components

--- src/components/Bookings/BookingsPage.js
+++ src/components/Bookings/BookingsPage.js
@@ -1,10 +1,20 @@
-import WeekPicker from "./WeekPicker";
+import { useState } from "react";
 
-const BookingsPage = () => (
-  <main className="bookings-page">
-    <p>Bookings!</p>
-    <WeekPicker date={new Date()} />
-  </main>
-);
+//import components
+import BookablesList from "../Bookables/BookablesList";
+import Bookings from "./Bookings";
+
+const BookingsPage = () => {
+  /**Manage state */
+  const [bookable, setBookable] = useState(null);
+
+  /**UI */
+  return (
+    <main className="bookings-page">
+      <BookablesList bookable={bookable} setBookable={setBookable} />
+      <Bookings bookable={bookable} />
+    </main>
+  );
+};
 
 export default BookingsPage;
