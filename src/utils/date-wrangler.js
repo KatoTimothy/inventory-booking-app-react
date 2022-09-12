4f0ad0630cee9a11c810b144301f67be11e0db73
unknown <katotimuci@gmail.com>
2022-09-12 11:13:50 +0300

    Created shortISO function
    Returns a date part of ISO string of a certain date

--- src/utils/date-wrangler.js
+++ src/utils/date-wrangler.js
@@ -14,3 +14,8 @@ export const getWeek = (forDate, daysOffset = 0) => {
     end: shiftDate(date, 6 - day),
   };
 };
+
+//returns date in string format 2022-09-20
+export const shortISO = (date) => {
+  return date.toISOString().split("T")[0];
+};
