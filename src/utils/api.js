07842b02858da67546ee32aa79683ccffca2a367
unknown <katotimuci@gmail.com>
2022-09-13 21:12:23 +0300

    Update api.js
    Renamed error throw when request fails

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,22 @@
+import { shortISO } from "./date-wrangler";
+
+export const getBookings = (bookableId, startDate, endDate) => {
+  const start = shortISO(startDate);
+  const end = shortISO(endDate);
+
+  const urlRoot = "http://localhost:3001/bookings";
+
+  const query = `bookableId=${bookableId}`;
+
+  //FETCH bookings from
+  //'htttp://localhost:3001/bookings/bookableId=1&date_gte=2022-06-21&date_lte=2022-06-27
+  return getData(`${urlRoot}?${query}`);
+};
+
 const getData = async (url) => {
   const res = await fetch(url);
   if (!res.ok) {
-    throw Error("There was a problem loading data.");
+    throw Error("Can't fetch data.");
   }
   return await res.json();
 };
