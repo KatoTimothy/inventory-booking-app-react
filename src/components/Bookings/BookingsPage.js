3072a24cc6e394afb4f863b5b25328351611bb9f
unknown <katotimuci@gmail.com>
2023-09-28 10:23:57 +0300

    Update BookingsPage.js
    Made A little clean up

--- src/components/Bookings/BookingsPage.js
+++ src/components/Bookings/BookingsPage.js
@@ -1,14 +1,11 @@
 import { useState } from "react";
 
-//import components
 import BookablesList from "../Bookables/BookablesList";
 import Bookings from "./Bookings";
 
 const BookingsPage = () => {
-  /**Manage state */
   const [bookable, setBookable] = useState(null);
 
-  /**UI */
   return (
     <main className="bookings-page">
       <Bookings bookable={bookable} />
