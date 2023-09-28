d7b6f86d99c244af12ef77a5495cd9b64724d108
unknown <katotimuci@gmail.com>
2023-09-28 18:39:04 +0300

    Update BookablesList.js
    Data loading process is now done from its immediate parent component

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -1,23 +1,17 @@
-import { useEffect } from "react";
-
 import { FaArrowRight } from "react-icons/fa";
-import Spinner from "../UI/Spinner";
 
-import getData from "../../utils/api";
-
-//Action creator functions
-//that return actions used to update component state
+/**
+ * import action creator functions that 
+ * will return actions used to update component state
+ * */
 import {
-  fetchBookablesRequest,
-  fetchBookablesSuccess,
-  fetchBookablesError,
   nextBookable,
   setGroup,
   setBookableIndex,
 } from "../../reducers/action-creators";
 
 const BookablesList = ({ setBookable, state, dispatch }) => {
-  const { group, bookableIndex, bookables, isLoading, error } = state;
+  const { group, bookableIndex, bookables } = state;
   //List of all group names bookables belong to
   const groups = [...new Set(bookables.map((b) => b.group))];
 
@@ -36,71 +30,38 @@ const BookablesList = ({ setBookable, state, dispatch }) => {
     dispatch(setGroup(group));
   }
 
-  /**effects */
-  //Loads bookables data only once: when the component mounts
-  useEffect(() => {
-    //url containing bookables data
-    const url = "http://localhost:3001/bookables";
-
-    async function fetchData(dataUrl) {
-      try {
-        dispatch(fetchBookablesRequest());
-        const data = await getData(dataUrl);
-        dispatch(fetchBookablesSuccess(data));
-      } catch (error) {
-        dispatch(fetchBookablesError(error.message));
-      }
-    }
-    fetchData(url);
-  }, []);
-
-  /**UI */
-  if (error) {
-    return <p className="bookingsError">{error}</p>;
-  }
-
-  //display loading UI (spinner) if data hasn't arrived
-  if (isLoading) {
-    return (
-      <p>
-        <Spinner /> Loading bookables
-      </p>
-    );
-  }
   return (
-    <>
-      <div>
-        {/* groups selector */}
-        <select
-          name="group"
-          value={group}
-          onChange={(e) => onChangeGroup(e.target.value)}
-        >
-          {groups.map((g) => (
-            <option key={g}>{g}</option>
-          ))}
-        </select>
+    <div>
+      {/* groups selector */}
+      <select
+        name="group"
+        value={group}
+        onChange={(e) => onChangeGroup(e.target.value)}
+      >
+        {groups.map((g) => (
+          <option key={g}>{g}</option>
+        ))}
+      </select>
 
-        {/* Bookables list in the current group */}
-        <ul className="bookables items-list-nav">
-          {bookablesInGroup.map((b, i) => (
-            <li key={b.id} className={i === bookableIndex ? "selected" : null}>
-              <button className="btn" onClick={() => onChangeBookable(i)}>
-                {b.title}
-              </button>
-            </li>
-          ))}
-        </ul>
+      {/* Bookables list in the current group */}
+      <ul className="bookables items-list-nav">
+        {bookablesInGroup.map((b, i) => (
+          <li key={b.id} className={i === bookableIndex ? "selected" : null}>
+            <button className="btn" onClick={() => onChangeBookable(i)}>
+              {b.title}
+            </button>
+          </li>
+        ))}
+      </ul>
 
-        {/* Next button */}
-        <p>
-          <button className="btn" onClick={onClickNextButton}>
-            <FaArrowRight />
-            <span>Next</span>
-          </button>
-        </p>
-      </div>
-    </>
+      {/* Next button */}
+      <p>
+        <button className="btn" onClick={onClickNextButton}>
+          <FaArrowRight />
+          <span>Next</span>
+        </button>
+      </p>
+    </div>
   );
 };
 
