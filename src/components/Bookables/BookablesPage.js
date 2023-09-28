8ffc1009637ecd15b4e74c5448cce0a635dcf919
unknown <katotimuci@gmail.com>
2023-09-28 18:40:36 +0300

    Update BookablesPage.js
    The component now loads  bookables data, manages it and makes it available to its children.

--- src/components/Bookables/BookablesPage.js
+++ src/components/Bookables/BookablesPage.js
@@ -1,9 +1,21 @@
-import React, { useReducer, useState } from "react";
+import React, { useReducer, useState, useEffect } from "react";
+import Spinner from "../UI/Spinner";
+
 import BookablesList from "./BookablesList";
 import BookableDetails from "./BookablesDetails";
 
+import getData from "../../utils/api";
+
 //reducer to manage component state with actions
 import bookablesReducer from "../../reducers/bookables-reducer";
+
+//Action creator functions
+//that return actions used to update component state
+import {
+  fetchBookablesRequest,
+  fetchBookablesSuccess,
+  fetchBookablesError,
+} from "../../reducers/action-creators";
 
 //initial state for the reducer
 const initialState = {
@@ -18,18 +30,59 @@ const initialState = {
 export const BookablesPage = () => {
   const [bookable, setBookable] = useState();
   const [state, dispatch] = useReducer(bookablesReducer, initialState);
-  return (
+  const { isLoading, error } = state;
+
+  //Loads bookables data only once: when the component mounts
+  useEffect(() => {
+    //url containing bookables data
+    const url = "http://localhost:3001/bookables";
+
+    /**effects */
+    //fetches data only once. That is, when the component mounts
+    async function fetchData(dataUrl) {
+      try {
+        dispatch(fetchBookablesRequest());
+        const data = await getData(dataUrl);
+        dispatch(fetchBookablesSuccess(data));
+      } catch (error) {
+        dispatch(fetchBookablesError(error.message));
+      }
+    }
+    fetchData(url);
+  }, []);
+
+  /**UI */
+  //loading spinner renders if data hasn't arrived
+  if (isLoading) {
+    return (
+      <main className="bookables-page">
+        <p>
+          <Spinner /> Loading bookables
+        </p>
+      </main>
+    );
+  }
+  //error page displays if error occurs during data fetch
+  if (error) {
     <main className="bookables-page">
-      <BookablesList
-        setBookable={setBookable}
-        state={state}
-        dispatch={dispatch}
-      />
-      <BookableDetails
-        bookable={bookable}
-        hasDetails={state.hasDetails}
-        dispatch={dispatch}
-      />
-    </main>
+      <p className="bookingsError">{error}</p>
+    </main>;
+  }
+
+  return (
+    <>
+      <main className="bookables-page">
+        <BookablesList
+          setBookable={setBookable}
+          state={state}
+          dispatch={dispatch}
+        />
+        <BookableDetails
+          bookable={bookable}
+          hasDetails={state.hasDetails}
+          dispatch={dispatch}
+        />
+      </main>
+    </>
   );
 };
