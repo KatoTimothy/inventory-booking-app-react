91a1758edce6936683adc3d611aca2413a99d52a
unknown <katotimuci@gmail.com>
2023-09-28 12:25:00 +0300

    Update BookablesPage.js
    Component now manages state of its children

--- src/components/Bookables/BookablesPage.js
+++ src/components/Bookables/BookablesPage.js
@@ -1,13 +1,35 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import BookablesList from "./BookablesList";
 import BookableDetails from "./BookablesDetails";
 
+//reducer to manage component state with actions
+import bookablesReducer from "../../reducers/bookables-reducer";
+
+//initial state for the reducer
+const initialState = {
+  group: "Rooms",
+  bookableIndex: 0,
+  bookables: [],
+  isLoading: true,
+  hasDetails: true,
+  error: "",
+};
+
 export const BookablesPage = () => {
   const [bookable, setBookable] = useState();
+  const [state, dispatch] = useReducer(bookablesReducer, initialState);
   return (
     <main className="bookables-page">
-      <BookablesList setBookable={setBookable} />
-      <BookableDetails bookable={bookable} />
+      <BookablesList
+        setBookable={setBookable}
+        state={state}
+        dispatch={dispatch}
+      />
+      <BookableDetails
+        bookable={bookable}
+        hasDetails={state.hasDetails}
+        dispatch={dispatch}
+      />
     </main>
   );
 };
