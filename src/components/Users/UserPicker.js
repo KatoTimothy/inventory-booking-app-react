53ae6f261ba3e00e0ad7c28bcdd6b488fc5717d8
unknown <katotimuci@gmail.com>
2023-09-28 10:45:13 +0300

    Update UserPicker.js

--- src/components/Users/UserPicker.js
+++ src/components/Users/UserPicker.js
@@ -1,25 +1,31 @@
 import { useState, useEffect } from "react";
 import Spinner from "../UI/Spinner";
+import getData from "../../utils/api";
 
 //import data from "../../static.json";
 //const { users } = data;
 
 const UserPicker = () => {
-  const [users, setUsers] = useState(null);
+  const [users, setUsers] = useState([]);
+  const [userIndex, setUserIndex] = useState(0);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const fetchUsers = async () => {
-      const response = await fetch("http://localhost:3001/users");
-      const data = await response.json();
-      setUsers(data);
-    };
-    fetchUsers();
+    setIsLoading(true);
+    getData("http://localhost:3001/users")
+      .then((userData) => {
+        setUsers(userData);
+        setIsLoading(false);
+      })
+      .catch((e) => {
+        setError(e.message);
+      });
   }, []);
 
-  if (users === null) {
+  if (isLoading) {
     return <Spinner />;
   }
-
   return (
     <select>
       {users.map(({ name, id }) => (
