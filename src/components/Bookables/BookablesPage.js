b74f2d1e013dd7a74ce1893f9a35be4be099eabd
unknown <katotimuci@gmail.com>
2023-09-28 10:23:16 +0300

    Update BookablesPage.js
    Moved the BookablesList and BookableDetails to enforce this Single responsibility principle

--- src/components/Bookables/BookablesPage.js
+++ src/components/Bookables/BookablesPage.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import BookablesList from "./BookablesList";
+import BookableDetails from "./BookablesDetails";
 
 export const BookablesPage = () => {
+  const [bookable, setBookable] = useState();
   return (
     <main className="bookables-page">
-      <BookablesList />
+      <BookablesList setBookable={setBookable} />
+      <BookableDetails bookable={bookable} />
     </main>
   );
 };
