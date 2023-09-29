9ff3c2b02b48490c837aabd168fa39b57818ee8d
KatoTimothy <katotimuci@gmail.com>
2023-09-29 11:25:46 +0300

    Update UsersList.js
    The component now makes use of the global state exposed by the UserProvider component via useContext api

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -1,11 +1,13 @@
+import { useContext } from "react";
 import { setUserIndex } from "../../reducers/action-creators";
+import { UserContext } from "./UserProvider";
 
-const UsersList = ({ state, dispatch }) => {
-  /**Manage state */
+const UsersList = () => {
+  const { state, dispatch } = useContext(UserContext);
   const { users, userIndex } = state;
 
   //Event handlers
-  function onSelectUser(index) {
+  function handleOnSelectUser(index) {
     dispatch(setUserIndex(index));
   }
   return (
@@ -13,7 +15,7 @@ const UsersList = ({ state, dispatch }) => {
       <ul className="users items-list-nav">
         {users.map((u, i) => (
           <li className={i === userIndex ? "selected" : null} key={u.id}>
-            <button className="btn" onClick={() => onSelectUser(i)}>
+            <button className="btn" onClick={() => handleOnSelectUser(i)}>
               {u.name}
             </button>
           </li>
