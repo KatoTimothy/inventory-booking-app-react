2c3ec2cabf2451675008df7b635b7842e0f7f43e
KatoTimothy <katotimuci@gmail.com>
2023-09-29 11:21:50 +0300

    Update App.js
    Added a UserProvide component that handles and manages user data throughout the app

--- src/components/App.js
+++ src/components/App.js
@@ -7,45 +7,48 @@ import UserPicker from "./Users/UserPicker";
 import BookingsPage from "./Bookings/BookingsPage";
 import { BookablesPage } from "./Bookables/BookablesPage";
 import UsersPage from "./Users/UsersPage";
+import UserProvider from "./Users/UserProvider";
 
 const App = () => (
   <Router>
-    <div className="App">
-      <header>
-        <nav>
-          <ul>
-            {/* Bookings  */}
-            <li>
-              <Link to="/bookings" className="btn btn-header">
-                <FaCalendarAlt />
-                <span>Bookings</span>
-              </Link>
-            </li>
-            {/* Bookables link */}
-            <li>
-              <Link to="/bookables" className="btn btn-header">
-                <FaDoorOpen />
-                <span>Bookables</span>
-              </Link>
-            </li>
-            {/* Users link */}
-            <li>
-              <Link to="/users" className="btn btn-header">
-                <FaUsers />
-                <span>Users</span>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <UserPicker />
-      </header>
-      <Routes>
-        <Route path="/" element={<BookablesPage />} />
-        <Route path="/bookings" element={<BookingsPage />} />
-        <Route path="/bookables" element={<BookablesPage />} />
-        <Route path="/users" element={<UsersPage />} />
-      </Routes>
-    </div>
+    <UserProvider>
+      <div className="App">
+        <header>
+          <nav>
+            <ul>
+              {/* Bookings  */}
+              <li>
+                <Link to="/bookings" className="btn btn-header">
+                  <FaCalendarAlt />
+                  <span>Bookings</span>
+                </Link>
+              </li>
+              {/* Bookables link */}
+              <li>
+                <Link to="/bookables" className="btn btn-header">
+                  <FaDoorOpen />
+                  <span>Bookables</span>
+                </Link>
+              </li>
+              {/* Users link */}
+              <li>
+                <Link to="/users" className="btn btn-header">
+                  <FaUsers />
+                  <span>Users</span>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <UserPicker />
+        </header>
+        <Routes>
+          <Route path="/" element={<BookablesPage />} />
+          <Route path="/bookings" element={<BookingsPage />} />
+          <Route path="/bookables" element={<BookablesPage />} />
+          <Route path="/users" element={<UsersPage />} />
+        </Routes>
+      </div>
+    </UserProvider>
   </Router>
 );
 
