b979f9584412e3639c616f70264d0e176dcf40f6
KatoTimothy <katotimuci@gmail.com>
2023-09-29 11:26:59 +0300

    Update UsersPage.js
    Moved state management logic to the UserProvider component that now exposes the user data globally through out the app

--- src/components/Users/UsersPage.js
+++ src/components/Users/UsersPage.js
@@ -1,49 +1,50 @@
-import React, { useReducer, useEffect } from "react";
+import React, { useReducer, useEffect, useContext } from "react";
 
 import UsersList from "./UsersList";
 import UserDetails from "./UserDetails";
 import Spinner from "../UI/Spinner";
+import { UserContext } from "./UserProvider";
 
-import usersReducer from "../../reducers/users-reducer";
-import {
-  setUserIndex,
-  fetchUsersError,
-  fetchUsersRequest,
-  fetchUsersSuccess,
-  nextUser,
-} from "../../reducers/action-creators";
+// import usersReducer from "../../reducers/users-reducer";
+// import {
+//   setUserIndex,
+//   fetchUsersError,
+//   fetchUsersRequest,
+//   fetchUsersSuccess,
+//   nextUser,
+// } from "../../reducers/action-creators";
 
-import getData from "../../utils/api";
+// import getData from "../../utils/api";
 
-const initialState = {
-  isLoading: true,
-  users: [],
-  error: "",
-  userIndex: 0,
-};
+// const initialState = {
+//   isLoading: true,
+//   users: [],
+//   error: "",
+//   userIndex: 0,
+// };
 export default function UsersPage() {
-  const [state, dispatch] = useReducer(usersReducer, initialState);
-
+  // const [state, dispatch] = useReducer(usersReducer, initialState);
+  const { state, dispatch } = useContext(UserContext);
   const { isLoading, error } = state;
 
   /**Effects */
-  useEffect(() => {
-    //fetch data from server
-    const url = "http://localhost:3001/users";
+  // useEffect(() => {
+  //   //fetch data from server
+  //   const url = "http://localhost:3001/users";
 
-    async function fetchData(dataUrl) {
-      try {
-        dispatch(fetchUsersRequest());
-        const data = await getData(dataUrl);
-        if (data) {
-          dispatch(fetchUsersSuccess(data));
-        }
-      } catch (error) {
-        dispatch(fetchUsersError(error.message));
-      }
-    }
-    fetchData(url);
-  }, []);
+  //   async function fetchData(dataUrl) {
+  //     try {
+  //       dispatch(fetchUsersRequest());
+  //       const data = await getData(dataUrl);
+  //       if (data) {
+  //         dispatch(fetchUsersSuccess(data));
+  //       }
+  //     } catch (error) {
+  //       dispatch(fetchUsersError(error.message));
+  //     }
+  //   }
+  //   fetchData(url);
+  // }, []);
   /** UI */
   if (error) {
     return (
