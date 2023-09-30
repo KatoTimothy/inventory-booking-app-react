00cc84ce196ba445a329d4aed0b7801c701cc9d8
KatoTimothy <katotimuci@gmail.com>
2023-09-30 20:26:11 +0300

    Update UserDetails.js
    Component now makes use of the globally shared user Index to show selected user.

--- src/components/Users/UserDetails.js
+++ src/components/Users/UserDetails.js
@@ -1,18 +1,8 @@
-import { useContext } from "react";
-import { UserContext } from "./UserProvider";
-import UserPicker from "./UserPicker";
-
-const UserDetails = () => {
-  const { state } = useContext(UserContext);
-
-  const { users, userIndex } = state;
-  const user = users[userIndex];
-
+const UserDetails = ({ user }) => {
   return user ? (
     <div className="item user ">
       <div className="item-header">
         <h2>{user.name}</h2>
-        <UserPicker />
       </div>
       <div className="item-details">
         <h3>{user.title}</h3>
