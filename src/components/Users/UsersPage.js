2720ff30d08b9031a91c1400b5bbfa77aadfff47
KatoTimothy <katotimuci@gmail.com>
2023-10-03 13:37:44 +0300

    Update UsersPage.js
    No props are passed to the children.

--- src/components/Users/UsersPage.js
+++ src/components/Users/UsersPage.js
@@ -1,14 +1,11 @@
 import UsersList from "./UsersList";
 import UserDetails from "./UserDetails";
-import { useState } from "react";
 
 export default function UsersPage() {
-  const [selectedUser, setSelectedUser] = useState(null);
-
   return (
     <main className="users-page">
-      <UsersList setSelectedUser={setSelectedUser} />
-      <UserDetails user={selectedUser} setSelectedUser={setSelectedUser} />
+      <UsersList />
+      <UserDetails />
     </main>
   );
 }
