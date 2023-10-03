b038e13ac4dfd41f02a4b28bc8f9d9abd920d80e
KatoTimothy <katotimuci@gmail.com>
2023-10-03 13:36:53 +0300

    Update UsersList.js
    The component uses updated actions to dispatch actions to update state

--- src/components/Users/UsersList.js
+++ src/components/Users/UsersList.js
@@ -8,10 +8,9 @@ import getData from "../../utils/api";
 
 import usersReducer from "../../reducers/users-reducer";
 import {
-  setUserIndex,
-  fetchUsersError,
-  fetchUsersRequest,
-  fetchUsersSuccess,
+  dataRequestInitiated,
+  dataRequestSuccessful,
+  dataRequestFailed,
 } from "../../reducers/action-creators";
 
 //initial state for user reducer
@@ -19,19 +18,16 @@ const initialState = {
   isLoading: true,
   error: "",
   users: [],
-  userIndex: 0,
 };
-const UsersList = ({ setSelectedUser }) => {
+const UsersList = () => {
   const [state, dispatch] = useReducer(usersReducer, initialState);
   const { users, isLoading, error } = state;
 
-  const { index, setIndex } = useContext(UserContext);
-  setSelectedUser(users[index]);
+  const { user, setUser } = useContext(UserContext);
 
-  //Event handlers
-  function handleOnSelectUser(i) {
-    dispatch(setUserIndex(i));
-    setIndex(i);
+  /**Event handlers */
+  function handleOnSelectUser(user) {
+    setUser(user);
   }
 
   /**Effects */
@@ -40,15 +36,16 @@ const UsersList = ({ setSelectedUser }) => {
     const uri = "http://localhost:3001/users";
     async function fetchUserData(uri) {
       try {
-        dispatch(fetchUsersRequest());
+        dispatch(dataRequestInitiated());
         const data = await getData(uri);
-        data && dispatch(fetchUsersSuccess(data));
+        dispatch(dataRequestSuccessful(data));
+        setUser(data[0]);
       } catch (error) {
-        dispatch(fetchUsersError(error.message));
+        dispatch(dataRequestFailed(error.message));
       }
     }
     fetchUserData(uri);
-  }, []);
+  }, [setUser]);
 
   /**UI */
   if (isLoading) {
@@ -61,18 +58,20 @@ const UsersList = ({ setSelectedUser }) => {
   if (error) {
     return <div className="bookigsErrror">{error}</div>;
   }
-  return (
+  return user ? (
     <div>
       <ul className="users items-list-nav">
         {users.map((u, i) => (
-          <li className={i === index ? "selected" : null} key={u.id}>
-            <button className="btn" onClick={() => handleOnSelectUser(i)}>
+          <li className={u.id === user.id ? "selected" : null} key={i}>
+            <button className="btn" onClick={() => handleOnSelectUser(u)}>
               {u.name}
             </button>
           </li>
         ))}
       </ul>
     </div>
+  ) : (
+    <div className="bookingsError">Oops user seems to be null</div>
   );
 };
 
