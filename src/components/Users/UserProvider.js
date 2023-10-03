9deca47cd3a271244f702641ce4a8c7bb9d4d525
KatoTimothy <katotimuci@gmail.com>
2023-10-03 13:39:17 +0300

    Update UserProvider.js
    The UserProvider now exposes current user object to all its children

--- src/components/Users/UserProvider.js
+++ src/components/Users/UserProvider.js
@@ -1,16 +1,16 @@
 import { createContext, useState } from "react";
 
 export const UserContext = createContext({
-  index: null,
-  setIndex: null,
+  user: null,
+  setUser: null,
 });
 
 //Component exposes the selected user index
 //to all its nested components
 const UserProvider = ({ children }) => {
-  const [index, setIndex] = useState(0);
+  const [user, setUser] = useState(null);
   return (
-    <UserContext.Provider value={{ index, setIndex }}>
+    <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
   );
